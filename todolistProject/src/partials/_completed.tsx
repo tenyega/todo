f4bf87c0950e4ee86c3960a3b99b856b431f4d14
yega <mdolma@ymail.com>
2024-07-09 23:45:20 +0200

    margin to tasks pending and completed

--- todolistProject/src/partials/_completed.tsx
+++ todolistProject/src/partials/_completed.tsx
@@ -35,9 +35,9 @@ export default function Completed( props: { message: any; }) {
     console.log(resToDo)
     return (
         <>
-          <div className="flex  flex-row gap-5 px-5 py-10 ">
+          <div className="flex  flex-row gap-5 px-5 py-10 ml-38 pl-28">
           
-            <div className="flex flex-row  min-h-screen gap-5 px-5 py-20 bg-gray-100 flex-wrap">
+            <div className="flex flex-row  min-h-screen gap-5 px-5 py-20 bg-gray-100 flex-wrap ml-38 pl-28">
     
                 {resToDo.map((data: { id: Key | null | undefined, title: string, date: string, desc: string, priority: string, status: string })  =>
                     data.status === message ? (
@@ -52,7 +52,7 @@ export default function Completed( props: { message: any; }) {
                                 <h3 className="text-gray-600 text-md">Priority: {data.priority}</h3>
                                 <h3 className="text-gray-600 text-md">Status: {data.status}</h3>
                                 <div className="flex mt-4 md:mt-6">
-                    <button onClick={()=>handelete(data.id)}  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button>
+                    <button onClick={()=>handelete(data.id)}  className="text-center inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button>
                 </div>        
                         </div>
                     ) : null
