1bc6285a30c1ccb8827aa71eaf706a76e47141b8
yega <mdolma@ymail.com>
2024-07-11 10:58:27 +0200

    chnages

--- todolistProject/src/components/NavBar.tsx
+++ todolistProject/src/components/NavBar.tsx
@@ -28,15 +28,15 @@ export default function NavBar(props:any) {
                   <path d="M18 0H6a2 2 0 0 0-2 2h14v12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Z"/>
                   <path d="M14 4H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2ZM2 16v-6h12v6H2Z"/>
                </svg>
-                      <span className="flex-1 ms-3 whitespace-nowrap  text-gray-500">MES TÂCHES </span>
-                      <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-black-800 bg-black-100 rounded-full dark:bg-orange-500 dark:text-black-300">9 </span>
+                    <span className="flex-1 ms-3 whitespace-nowrap  text-gray-500">MES TÂCHES </span>
+                     <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-green-800 bg-green-100 rounded-full dark:bg-green-500 dark:text-green-300">9 </span>
          </NavLink>       
          <NavLink to="/important" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                   <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
                   <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z"/>
                </svg>
                       <span className="flex-1 ms-2 whitespace-nowrap  text-gray-500"> IMPORTANTES </span>
-                      <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-black-800 bg-black-100 rounded-full dark:bg-red-500 dark:text-black-300">2 </span>
+                      <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-green-800 bg-green-100 rounded-full dark:bg-green-500 dark:text-green-300">2 </span>
           </NavLink> 
           <NavLink to="/contact" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                       <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
