1897028929518b6357aebd416e2529724d89124f
yega <mdolma@ymail.com>
2024-07-11 11:32:25 +0200

    error in add

--- todolistProject/src/components/menu.tsx
+++ todolistProject/src/components/menu.tsx
@@ -6,7 +6,7 @@ export default function Menu() {
         <div className="p-4 sm:ml-64">
     <button className="text-white bg-gradient-to-r from-red-400 via-orange-500 to-yellow-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
     <span className="">
-    <NavLink to="/add " className="text-white">AJOUTER UNE TÂCHE</NavLink> 
+    <NavLink to="/add" className="text-white">AJOUTER UNE TÂCHE</NavLink> 
 
         </span>
     </button>
